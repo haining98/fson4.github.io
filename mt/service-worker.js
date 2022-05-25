444e3e5201721c6e058fadc4cbbc748c3e817ed0
fson4 <76850089+fson4@users.noreply.github.com>
2022-05-26 00:22:16 +0200

    MT

---
+++ mt/service-worker.js
@@ -0,0 +1,13 @@
+const id = "MT*22.05.26",
+	data = [
+		"/mt/",
+		"app.webmanifest",
+		"resource/font/inter.woff2",
+		"resource/style.css",
+		"resource/script.js",
+		"resource/img/icon.svg",
+		"resource/img/icon.png"
+	]
+self.addEventListener("install", e => e.waitUntil(caches.open(id).then(c => c.addAll(data))))
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(key => { if (/MT/.test(key) && key !== id) return caches.delete(key) })))))
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))))
---
+++ nb/service-worker.js
@@ -0,0 +1,13 @@
+const id = "NB*22.05.26",
+	data = [
+		"/nb/",
+		"app.webmanifest",
+		"resource/font/inter.woff2",
+		"resource/style.css",
+		"resource/script.js",
+		"resource/img/icon.svg",
+		"resource/img/icon.png"
+	]
+self.addEventListener("install", e => e.waitUntil(caches.open(id).then(c => c.addAll(data))))
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(key => { if (/NB/.test(key) && key !== id) return caches.delete(key) })))))
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))))
